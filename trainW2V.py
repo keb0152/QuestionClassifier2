7e74d0c531b0c77101edd05f1bfa7c083bd53d36
keb0152 <31708873+keb0152@users.noreply.github.com>
2018-05-29 15:10:54 -0400

    Update trainW2V.py

--- trainW2V.py
+++ trainW2V.py
@@ -6,7 +6,7 @@ logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=lo
 
 # train word2vec on the two sentences
 model = gensim.models.Word2Vec(sentences, min_count=100, size=100)
-saveLocation=open('W2VBrown10_100','w')
+saveLocation=open('Models/W2VBrown10_100','w')
 # model.save(saveLocation)
 model.wv.save_word2vec_format(saveLocation)
 model.accuracy('Data/assessment')
